e8b17f56b523c5f87738c83526f0f15521b80c7a
James Sapp <jameseisapp@gmail.com>
2020-11-18 00:03:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,7 +36,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.any? do |word| 
+  planeteer_calls.find do |word| 
     valid_calls.include? word
   end 
    
